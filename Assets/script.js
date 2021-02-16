5e18a94cde603fff9208d5d402d91888f5fec3fa
ThatSkiPatroller <69833928+ThatSkiPatroller@users.noreply.github.com>
2021-02-16 02:49:23 -0600

    added bio click function

--- Assets/script.js
+++ Assets/script.js
@@ -3,23 +3,33 @@ document.getElementById("contact").addEventListener("click", function () {
     window.location.pathname = "C:/Users/Trace/codeWork/ThatSkiPatroller.github.io-Portfolio/Develop/Contact.html";
 });
 
+// document.getElementById("biography").addEventListener("click", function () {
+//     var id = null;
+//     var can = document.getElementById("biography");
+//     clearInterval(id);
+//     var x = 0;
+//     id = setInterval(frame, -50);
+//     function frame () {
+//         if (x == 360) {
+//             clearInterval(id);
+//         } else {
+//             x++;
+//             can.style.top = x + "px";
+//             // can.style.top = x + "px";
+//         }
+//     }
+//     var bio = document.createElement("div");
+
+
+
+// })
+
 document.getElementById("biography").addEventListener("click", function () {
-    var id = null;
-    var can = document.getElementById("biography");
-    clearInterval(id);
-    var x = 0;
-    id = setInterval(frame, -50);
-    function frame () {
-        if (x == 360) {
-            clearInterval(id);
-        } else {
-            x++;
-            can.style.top = x + "px";
-            // can.style.top = x + "px";
-        }
-    }
-    var bio = document.createElement("div");
-
-
-
+    var bio = document.createElement("ul");
+    bio.classList.add("collection");
+    var list = document.createElement("li");
+    list.classList.add("collection-item");
+    list.innerHTML= "My bio is this dude";
+    document.getElementById("below").appendChild(bio);
+    document.getElementById("below").appendChild(list);
 })
--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Portfolio
 
 # User Story
-As a user, I would like to be able to easily view all of the background information of this individual, as well as be able to see links to past projects as well as their contact information.
+As a user, I would like to be able to easily view all of the background information of this individual, as well as be able to see links to past projects and as their contact information.
 
