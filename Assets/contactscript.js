761a14ab8e91ff08dad47daff1a93aeafc1a6bff
ThatSkiPatroller <69833928+ThatSkiPatroller@users.noreply.github.com>
2021-02-16 23:37:17 -0600

    last html fix worked, fixed back button html href

--- Assets/contactscript.js
+++ Assets/contactscript.js
@@ -1,3 +1,3 @@
 document.getElementById("btn").addEventListener("click", function () {
-    window.location.pathname = "C:/Users/Trace/codeWork/ThatSkiPatroller.github.io-Portfolio/index.html";
+    window.location.href = "https://thatskipatroller.github.io/ThatSkiPatroller.github.io-Portfolio/";
 });
