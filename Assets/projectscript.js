4eae41af3b6f7656677c86880337e3d5dbb73e14
ThatSkiPatroller <69833928+ThatSkiPatroller@users.noreply.github.com>
2021-04-22 14:19:15 -0500

    Updated new projects

--- Assets/projectscript.js
+++ Assets/projectscript.js
@@ -6,6 +6,15 @@ document.getElementById("scheduler").addEventListener("click", function () {
     window.location.href = "https://thatskipatroller.github.io/ThatSkiPatroller.github.io-Day-Planner/";
 });
 
+document.getElementById("project2").addEventListener("click", function () {
+    window.location.href = "https://limitless-bayou-89275.herokuapp.com/";
+});
+
+document.getElementById("notetaker").addEventListener("click", function () {
+    window.location.href = "https://polar-beach-03042.herokuapp.com/";
+});
+
 document.getElementById("btn").addEventListener("click", function () {
     window.location.href = "https://thatskipatroller.github.io/ThatSkiPatroller.github.io-Portfolio/";
 });
+
